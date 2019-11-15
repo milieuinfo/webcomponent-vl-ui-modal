214201b4eca4b50efb03f44ae8eeb74dab88d48a
Tom Coemans <tom.coemans@gmail.com>
2019-11-15 15:02:00 +0100

    focus probleem in Safari opgelost

--- demo/vl-modal-test.js
+++ demo/vl-modal-test.js
@@ -1,26 +1,44 @@
-import {VlElement, define} from '/node_modules/vl-ui-core/vl-core.js';
-import '../vl-modal.src.js';
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
 import '/node_modules/vl-ui-button/vl-button.js';
 import '/node_modules/vl-ui-input-field/vl-input-field.js';
+import '../vl-modal.src.js';
 
 export class VlModalTest extends VlElement(HTMLElement) {
   constructor() {
     super(`
       <style>
-          @import '/node_modules/vl-ui-modal/style.css';
           @import '/node_modules/vl-ui-button/style.css';
           @import '/node_modules/vl-ui-input-field/style.css';
       </style>
 
       <vl-modal data-title="Modal" closable not-cancellable>
         <button is="vl-button" slot="button">aanvraag starten</button>
-        <input is="vl-input-field" slot="content"/></input>
-      </vl-modal>`);
+        <input is="vl-input-field" slot="content" block></input>
+      </vl-modal>
+    `);
   }
-  
+
   open() {
     this._element.open();
   }
 }
 
-define('vl-modal-test', VlModalTest);
+export class VlModalContainerTest extends VlElement(HTMLElement) {
+  constructor() {
+    super(`
+      <style>
+        @import '/node_modules/vl-ui-input-field/style.css';
+      </style>
+      <vl-modal-test></vl-modal-test>
+    `);
+  }
+
+  open() {
+    this._element.open();
+  }
+}
+
+customElements.whenDefined('vl-input-field').then(() => {
+  define('vl-modal-test', VlModalTest);
+  define('vl-modal-container-test', VlModalContainerTest);
+});
