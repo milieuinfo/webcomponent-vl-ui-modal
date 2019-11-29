6f7647c89c2ee48699b58dced045ab356ce0ca24
cambiph <incubusattax@gmail.com>
2019-11-29 16:47:58 +0100

    Initiele commit voor pagina objecten

--- test/e2e/modal.test.js
+++ test/e2e/modal.test.js
@@ -1,12 +1,26 @@
 
 const { assert, driver } = require('vl-ui-core').Test;
 const VlModalPage = require('./pages/vl-modal.page');
+const { By } = require('selenium-webdriver')
 
 describe('vl-modal', async () => {
     const vlModalPage = new VlModalPage(driver);
 
     before(() => {
         return vlModalPage.load();
+    });
+
+    it('ik kan de modal openen door op open te klikken en sluiten door op annuleer te klikken', async () => {
+        const openButton = await vlModalPage.getModalZonderButtonEnContentOpenKnop();
+        await openButton.click();
+
+        const modal = await vlModalPage.getModalZonderButtonEnContent();
+        await assert.eventually.isTrue(modal.isDisplayed());
+
+        const annuleer = await vlModalPage.getModalZonderButtonEnContentAnnuleerKnop();
+        await annuleer.click();
+
+        await assert.eventually.isFalse(modal.isDisplayed());
     });
     
     after(() => driver && driver.quit());
--- test/e2e/pages/vl-modal.page.js
+++ test/e2e/pages/vl-modal.page.js
@@ -2,9 +2,26 @@ const VlModal = require('../components/vl-modal');
 const { Page } = require('vl-ui-core');
 const { Config } = require('vl-ui-core');
 
+const { By } = require('selenium-webdriver');
+
 class VlModalPage extends Page {
     async _getModal(selector) {
         return new VlModal(this.driver, selector);
+    }
+
+    async getModalZonderButtonEnContent() {
+        const modal = await this._getModal('#modal-zb');
+        const shadowRoot = await modal.shadowRoot()
+        return shadowRoot.findElement(By.css('#modal-zb'));
+    }
+
+    async getModalZonderButtonEnContentOpenKnop() {
+        return this._getModal('#modal-zb-open');
+    }
+
+    async getModalZonderButtonEnContentAnnuleerKnop() {
+        const modal = await this.getModalZonderButtonEnContent();
+        return modal.findElement(By.css('#modal-toggle-cancellable'));
     }
 
     async load() {
