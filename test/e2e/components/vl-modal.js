8fc5af81564e3961d98df6a4a1dae69cb1dbea97
cambiph <incubusattax@gmail.com>
2019-11-29 16:59:58 +0100

    Verwijder vl-modal.js uit gitignore

---
+++ test/e2e/components/vl-modal.js
@@ -0,0 +1,9 @@
+const { VlElement } = require('vl-ui-core');
+
+class VlModal extends VlElement {  
+    constructor(driver, selector) {
+        super(driver, selector);
+    }
+}
+
+module.exports = VlModal;
