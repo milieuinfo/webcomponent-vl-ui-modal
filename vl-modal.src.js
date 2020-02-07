2b3c5e7491112c4ade4fe6e452105e5bc993109e
Sander Kleykens <sander.kleykens@avnu.be>
2020-02-07 15:23:52 +0100

    Fix @govflanders import

--- vl-modal.src.js
+++ vl-modal.src.js
@@ -2,8 +2,8 @@ import { VlElement, define } from 'vl-ui-core';
 import 'vl-ui-icon';
 import 'vl-ui-button';
 import 'vl-ui-action-group';
-import 'vl-ui-util/dist/js/util.min.js';
-import 'vl-ui-core/dist/js/core.min.js';
+import '@govflanders/vl-ui-util/dist/js/util.min.js';
+import '@govflanders/vl-ui-core/dist/js/core.min.js';
 import 'vl-ui-modal/dist/modal.js';
 
 /**
