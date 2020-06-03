f434e216fda808672718f2e6352e47bdf6155e4a
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 14:22:43 +0200

    ESlint

--- test/e2e/components/vl-modal.js
+++ test/e2e/components/vl-modal.js
@@ -1,84 +1,84 @@
-const { driver } = require('vl-ui-core').Test.Setup;
-const { VlElement } = require('vl-ui-core').Test;
-const { By } = require('selenium-webdriver');
+const {driver} = require('vl-ui-core').Test.Setup;
+const {VlElement} = require('vl-ui-core').Test;
+const {By} = require('selenium-webdriver');
 
-class VlModal extends VlElement {  
-    async isDisplayed() {
-        const dialog = await this._getDialog();
-        return dialog.isDisplayed();
-    }
+class VlModal extends VlElement {
+  async isDisplayed() {
+    const dialog = await this._getDialog();
+    return dialog.isDisplayed();
+  }
 
-    async isCancellable() {
-        return this._exists(() => this._getCancelButton());
-    }
+  async isCancellable() {
+    return this._exists(() => this._getCancelButton());
+  }
 
-    async cancel() {
-        const button = await this._getCancelButton();
-        return button.click();
-    }
+  async cancel() {
+    const button = await this._getCancelButton();
+    return button.click();
+  }
 
-    async isClosable() {
-        return this._exists(() => this._getCloseButton());
-    }
+  async isClosable() {
+    return this._exists(() => this._getCloseButton());
+  }
 
-    async close() {
-        const button = await this._getCloseButton();
-        return button.click();
-    }
+  async close() {
+    const button = await this._getCloseButton();
+    return button.click();
+  }
 
-    async isSubmittable() {
-        return this._exists(() => this._getActionButton());
-    }
+  async isSubmittable() {
+    return this._exists(() => this._getActionButton());
+  }
 
-    async submit() {
-        const button = await this._getActionButton();
-        return button.click();
-    }
+  async submit() {
+    const button = await this._getActionButton();
+    return button.click();
+  }
 
-    async getContentSlotElements() {
-        const slot = await this._getContent();
-        return this.getAssignedElements(slot);
-    }
+  async getContentSlotElements() {
+    const slot = await this._getContent();
+    return this.getAssignedElements(slot);
+  }
 
-    async isInViewport() {
-        const dialog = await this._getDialog();
-        return dialog.isInViewport();
-    }
-    
-    async scrollToTop() {
-        const dialog = await this._getDialog();
-        return dialog.scrollToTop();
-    }
+  async isInViewport() {
+    const dialog = await this._getDialog();
+    return dialog.isInViewport();
+  }
 
-    async _getDialog() {
-        const element = await this.shadowRoot.findElement(By.css('dialog'));
-        return new VlElement(driver, element);
-    }
+  async scrollToTop() {
+    const dialog = await this._getDialog();
+    return dialog.scrollToTop();
+  }
 
-    async _getCancelButton() {
-        return this.shadowRoot.findElement(By.css('#modal-toggle-cancellable'));
-    }
+  async _getDialog() {
+    const element = await this.shadowRoot.findElement(By.css('dialog'));
+    return new VlElement(driver, element);
+  }
 
-    async _getCloseButton() {
-        return this.shadowRoot.findElement(By.css('#close'));
-    }
+  async _getCancelButton() {
+    return this.shadowRoot.findElement(By.css('#modal-toggle-cancellable'));
+  }
 
-    async _getContent() {
-        return this.shadowRoot.findElement(By.css('slot[name="content"]'));
-    }
+  async _getCloseButton() {
+    return this.shadowRoot.findElement(By.css('#close'));
+  }
 
-    async _getActionButton() {
-        return this.findElement(By.css('[slot="button"]'));
-    }
+  async _getContent() {
+    return this.shadowRoot.findElement(By.css('slot[name="content"]'));
+  }
 
-    async _exists(getter) {
-        try {
-            await getter();
-            return true;
-        } catch (error) {
-            return false;
-        }
+  async _getActionButton() {
+    return this.findElement(By.css('[slot="button"]'));
+  }
+
+  async _exists(getter) {
+    try {
+      await getter();
+      return true;
+    } catch (error) {
+      return false;
     }
+  }
 }
 
 module.exports = VlModal;
--- test/e2e/pages/vl-modal-auto-open.page.js
+++ test/e2e/pages/vl-modal-auto-open.page.js
@@ -1,19 +1,18 @@
 const VlModal = require('../components/vl-modal');
-const { Page, Config } = require('vl-ui-core').Test;
+const {Page, Config} = require('vl-ui-core').Test;
 
 class VlModalAutoOpenPage extends Page {
-    async _getModal(identifier) {
-        return new VlModal(this.driver, identifier);
-    }
+  async _getModal(identifier) {
+    return new VlModal(this.driver, identifier);
+  }
 
-    async getAutoOpenModal() {
-        return this._getModal('#modal-vt');
-    }
+  async getAutoOpenModal() {
+    return this._getModal('#modal-vt');
+  }
 
-    async load() {
-        await super.load(Config.baseUrl + '/demo/vl-modal-auto-open.html');
-    }
-
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-modal-auto-open.html');
+  }
 }
 
 module.exports = VlModalAutoOpenPage;
