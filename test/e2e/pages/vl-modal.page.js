542bdfd7ba11b05a798795f08ffca5bf653e5099
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-06-09 13:21:20 +0200

    UIG-695 nieuwe versie util die versie demo gebruikt waar header en footer geskipped kunnen worden

--- test/e2e/pages/vl-modal.page.js
+++ test/e2e/pages/vl-modal.page.js
@@ -1,8 +1,6 @@
 const VlModal = require('../components/vl-modal');
 const {Page, Config, VlElement} = require('vl-ui-core').Test;
 const {By} = require('selenium-webdriver');
-const {VlHeader} = require('vl-ui-header').Test;
-const {VlFooter} = require('vl-ui-footer').Test;
 
 class VlModalPage extends Page {
   async _getModal(identifier) {
@@ -114,11 +112,7 @@ class VlModalPage extends Page {
   }
 
   async load() {
-    await super.load(Config.baseUrl + '/demo/vl-modal.html');
-    const header = await new VlHeader(this.driver);
-    const footer = await new VlFooter(this.driver);
-    await header.remove();
-    await footer.remove();
+    await super.load(Config.baseUrl + '/demo/vl-modal.html?no-header=true&no-footer=true');
   }
 }
 
