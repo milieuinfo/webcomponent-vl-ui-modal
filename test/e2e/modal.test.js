9ada3e281e84a93a131da0b4d3aa6119bfdb62d7
Tom Coemans <tom.coemans@gmail.com>
2020-12-02 11:39:29 +0100

    try to fix e2e test

--- test/e2e/modal.test.js
+++ test/e2e/modal.test.js
@@ -135,6 +135,7 @@ describe('vl-modal', async () => {
     await assert.eventually.isFalse(modal.isDisplayed());
     await vlModalPage.openModal();
     await assert.eventually.isTrue(modal.isDisplayed());
+    await modal.click();
     await modal.sendKeys(Key.ESCAPE);
     await assert.eventually.isTrue(modal.isDisplayed());
     await modal.cancel();
